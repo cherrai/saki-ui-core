57feed05f64996378959eb1198fdde50e737490f
aiiko <shiina@aiiko.club>
2022-07-10 02:50:25 +0800

    backup

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,18 @@
 import { snackbar } from './snackbar'
 import { alert, prompt } from './dialog'
 import { progressBar } from './dialog/progress'
-export { snackbar, alert, prompt, progressBar }
+import { bindEvent } from './common/bindEvent'
+export {
+  snackbar,
+  alert,
+  prompt,
+  progressBar
+  , bindEvent
+}
 export default {
 	snackbar,
 	alert,
 	prompt,
 	progressBar,
+	bindEvent,
 }
