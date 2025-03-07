e9c2c73e1891db188c6d0e92f6528f393d4a5a49
ShiinaAiiko <shiina@aiiko.club>
2025-03-07 17:43:32 +0800

    update 25.03.07

--- src/dialog/alert.ts
+++ src/dialog/alert.ts
@@ -1,60 +1,69 @@
 export const alert = ({
-	titleAvatar,
-	titleAvatarText,
-	title,
-	content,
-	cancelText,
-	confirmText,
-	autoHideDuration = 0,
-	flexButton = false,
-	onCancel,
-	onConfirm,
+  titleAvatar,
+  titleAvatarText,
+  title,
+  content,
+  cancelText,
+  confirmText,
+  autoHideDuration = 0,
+  flexButton = false,
+  onCancel,
+  onConfirm,
 }: {
-	titleAvatar?: string
-	titleAvatarText?: string
-	title: string
-	content: string
-	cancelText?: string
-	confirmText?: string
-	autoHideDuration?: number
-	flexButton?: boolean
-	onCancel?: () => void
-	onConfirm?: () => void
+  titleAvatar?: string
+  titleAvatarText?: string
+  title: string
+  content: string
+  cancelText?: string
+  confirmText?: string
+  autoHideDuration?: number
+  flexButton?: boolean
+  onCancel?: () => void
+  onConfirm?: () => void
 }) => {
-	let el: any
-	const api = {
-		open() {
-			el = document.createElement('saki-dialog-alert')
-			el['title'] = title
-			el['titleAvatar'] = titleAvatar || ''
-			el['titleAvatarText'] = titleAvatarText || ''
-			el['title'] = title
-			el['content'] = content
-			el['cancelText'] = cancelText
-			el['confirmText'] = confirmText
-			el['autoHideDuration'] = autoHideDuration
-			el['flexButton'] = flexButton
-			el.addEventListener('load', () => {
-				el.open()
-			})
-			// el.onclose = () => {
-			//   console.log("close")
-			// 	document.body.removeChild(el)
-			// }
-			el.addEventListener('close', () => {
-				document.body.removeChild(el)
-			})
-			el.addEventListener('cancel', () => {
-				onCancel?.()
-			})
-			el.addEventListener('confirm', () => {
-				onConfirm?.()
-			})
-			document.body.appendChild(el)
-		},
-		close() {
-			el?.close?.()
-		},
-	}
-	return api
+  let el: any
+  let isClouse = false
+  const api = {
+    open() {
+      el = document.createElement('saki-dialog-alert')
+      el['title'] = title
+      el['titleAvatar'] = titleAvatar || ''
+      el['titleAvatarText'] = titleAvatarText || ''
+      el['title'] = title
+      el['content'] = content
+      el['cancelText'] = cancelText
+      el['confirmText'] = confirmText
+      el['autoHideDuration'] = autoHideDuration
+      el['flexButton'] = flexButton
+      el.addEventListener('load', () => {
+        el.open()
+        isClouse = false
+      })
+      // el.onclose = () => {
+      //   console.log("close")
+      // 	document.body.removeChild(el)
+      // }
+      el.addEventListener('close', () => {
+        console.log('removeChild', el)
+        try {
+          !isClouse && document.body.removeChild(el)
+          isClouse = true
+        } catch (error) {
+          console.error(error)
+
+        }
+      })
+      el.addEventListener('cancel', () => {
+        onCancel?.()
+      })
+      el.addEventListener('confirm', () => {
+        onConfirm?.()
+      })
+      document.body.appendChild(el)
+    },
+    close() {
+      el?.close?.()
+    },
+  }
+  return api
 }
