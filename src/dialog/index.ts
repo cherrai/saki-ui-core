52a9e539eb5f68c4f452872e9739838a7bc44e96
ShiinaAiiko <shiina@aiiko.club>
2025-04-25 05:48:17 +0800

    update ...

--- src/dialog/index.ts
+++ src/dialog/index.ts
@@ -1,229 +1,260 @@
 export type Error = string
 
 export const prompt = ({
-	title,
-	cancelText,
-	confirmText,
-	autoHideDuration,
-	value,
-	subtitle,
-	placeholder,
-	errorColor,
-	errorFontSize,
-	onChange,
-	onCancel,
-	onConfirm,
-	autoCloseAfterButtonClick = true,
-	flexButton = false,
+  title,
+  cancelText,
+  confirmText,
+  autoHideDuration,
+  value,
+  subtitle,
+  placeholder,
+  errorColor,
+  errorFontSize,
+  onChange,
+  onCancel,
+  onConfirm,
+  autoCloseAfterButtonClick = true,
+  flexButton = false,
 }: {
-	title: string
-	cancelText?: string
-	confirmText?: string
-	autoHideDuration?: number
-	value?: string
-	subtitle?: string
-	placeholder?: string
-	errorColor?: string
-	errorFontSize?: string
-	onChange?: (value: string) => Error
-	onCancel?: () => void
-	onConfirm?: () => void
-	autoCloseAfterButtonClick?: boolean
-	flexButton?: boolean
+  title: string
+  cancelText?: string
+  confirmText?: string
+  autoHideDuration?: number
+  value?: string
+  subtitle?: string
+  placeholder?: string
+  errorColor?: string
+  errorFontSize?: string
+  onChange?: (value: string) => Error
+  onCancel?: () => void
+  onConfirm?: () => void
+  autoCloseAfterButtonClick?: boolean
+  flexButton?: boolean
 }) => {
-	let el: any
-	const api = {
-		open() {
-			if (el) {
-				el.open()
-				return
-			}
-			el = document.createElement('saki-dialog-prompt')
-			el['title'] = title || ''
-			el['value'] = value || ''
-			el['placeholder'] = placeholder || ''
-			el['errorColor'] = errorColor || ''
-			el['errorFontSize'] = errorFontSize || ''
-			el['cancelText'] = cancelText || ''
-			el['confirmText'] = confirmText || ''
-			el['subtitle'] = subtitle || ''
-			el['autoHideDuration'] = autoHideDuration || 0
-			el['autoCloseAfterButtonClick'] = autoCloseAfterButtonClick
-			el['flexButton'] = flexButton
-			// el.addEventListener('load', () => {
-			// 	el.open()
-			// })
-			// el.onclose = () => {
-			//   console.log("close")
-			// 	document.body.removeChild(el)
-			// }
-			el.addEventListener('close', () => {
-				document.body.contains(el) && document.body.removeChild(el)
-				el = null
-			})
-			el.addEventListener('cancel', () => {
-				onCancel && onCancel()
-			})
-			el.addEventListener('changevalue', (e: CustomEvent) => {
-				if (onChange) {
-					const err = onChange(e.detail)
-					el['error'] = err || ''
-				}
-			})
-			el.addEventListener('confirm', () => {
-				onConfirm && onConfirm()
-			})
-			document.body.appendChild(el)
-		},
-		setError(err: Error) {
-			el['error'] = err
-		},
-		close() {
-			el?.close && el?.close()
-		},
-	}
-	return api
+  let el: any
+  const api = {
+    open() {
+      if (el) {
+        el.open()
+        return
+      }
+      el = document.createElement('saki-dialog-prompt')
+      el['title'] = title || ''
+      el['value'] = value || ''
+      el['placeholder'] = placeholder || ''
+      el['errorColor'] = errorColor || ''
+      el['errorFontSize'] = errorFontSize || ''
+      el['cancelText'] = cancelText || ''
+      el['confirmText'] = confirmText || ''
+      el['subtitle'] = subtitle || ''
+      el['autoHideDuration'] = autoHideDuration || 0
+      el['autoCloseAfterButtonClick'] = autoCloseAfterButtonClick
+      el['flexButton'] = flexButton
+      // el.addEventListener('load', () => {
+      // 	el.open()
+      // })
+      // el.onclose = () => {
+      //   console.log("close")
+      // 	document.body.removeChild(el)
+      // }
+      el.addEventListener('close', () => {
+        document.body.contains(el) && document.body.removeChild(el)
+        el = null
+      })
+      el.addEventListener('cancel', () => {
+        onCancel && onCancel()
+      })
+      el.addEventListener('changevalue', (e: CustomEvent) => {
+        if (onChange) {
+          const err = onChange(e.detail)
+          el['error'] = err || ''
+        }
+      })
+      el.addEventListener('confirm', () => {
+        onConfirm && onConfirm()
+      })
+      document.body.appendChild(el)
+    },
+    setError(err: Error) {
+      el['error'] = err
+    },
+    close() {
+      el?.close && el?.close()
+    },
+  }
+  return api
+}
+
+export interface MultipleInput {
+  label: string
+  value?: string
+  type?: 'Text' | 'Password' | 'Number' | 'Textarea' | 'Search'
+  placeholderAnimation?: '' | 'MoveUp'
+  padding?: string
+  margin?: string
+  closeIcon?: boolean
+  border?: string
+  borderTop?: string
+  borderRight?: string
+  borderBottom?: string
+  borderLeft?: string
+  borderRadius?: string
+  subtitle?: string
+  placeholder?: string
+  width?: string
+  height?: string
+  maxLength?: number
+  error?: string
+  errorColor?: string
+  errorFontSize?: string
+  disabled?: boolean
+  onChange?: (value: string) => void
+  onClear?: () => void
 }
 
 export const multiplePrompts = ({
-	title,
-	content,
-	closeIcon,
-	autoHideDuration,
-	onOpen,
-	onClose,
-	multipleInputs,
-	flexButton = false,
-	buttons,
+  title,
+  content,
+  closeIcon,
+  autoHideDuration,
+  onOpen,
+  onClose,
+  multipleInputs,
+  flexButton = false,
+  buttons,
 }: {
-	title: string
-	content?: string
-	closeIcon?: boolean
-	autoHideDuration?: number
-	onClose?: () => void
-	onOpen?: () => void
+  title: string
+  content?: string
+  closeIcon?: boolean
+  autoHideDuration?: number
+  onClose?: () => void
+  onOpen?: () => void
 
-	multipleInputs: {
-		label: string
-		value?: string
-		type?: 'Text' | 'Password' | 'Number' | 'Textarea' | 'Search'
-		subtitle?: string
-		placeholder?: string
-		width?: string
-		height?: string
-		maxLength?: number
-		error?: string
-		errorColor?: string
-		errorFontSize?: string
-		disabled?: boolean
-		onChange?: (value: string) => void
-	}[]
+  multipleInputs: MultipleInput[]
 
-	flexButton?: boolean
-	buttons: {
-		label: string
-		text: string
-		width?: string
-		height?: string
-		fontSize?: string
-		border?: string
-		bgHoverColor?: string
-		bgActiveColor?: string
-		bgColor?: string
-		color?: string
-		borderRadius?: string
-		disabled?: boolean
-		loading?: boolean
-		loadingColor?: string
-		loadingWidth?: string
-		type?: 'Normal' | 'Primary'
-		autoCloseAfterButtonTap?: boolean
-		onTap?: () => void
-	}[]
+  flexButton?: boolean
+  buttons: {
+    label: string
+    text: string
+    width?: string
+    height?: string
+    fontSize?: string
+    border?: string
+    bgHoverColor?: string
+    bgActiveColor?: string
+    bgColor?: string
+    color?: string
+    borderRadius?: string
+    disabled?: boolean
+    loading?: boolean
+    loadingColor?: string
+    loadingWidth?: string
+    type?: 'Normal' | 'Primary'
+    autoCloseAfterButtonTap?: boolean
+    onTap?: () => void
+  }[]
 }) => {
-	let el: any
-	const api = {
-		open() {
-			if (el) {
-				el.open()
-				return
-			}
-			el = document.createElement('saki-dialog-multiple-prompts')
-			el['title'] = title || ''
-			el['content'] = content || ''
-			el['autoHideDuration'] = autoHideDuration || 0
-			el['flexButton'] = flexButton
-			el['closeIcon'] = closeIcon
-			el?.setMultipleInputs?.(multipleInputs)
-			el?.setButton?.(buttons)
-			// el.addEventListener('load', () => {
-			// 	el.open()
-			// })
-			// el.onclose = () => {
-			//   console.log("close")
-			// 	document.body.removeChild(el)
-			// }
-			let timer: number
-			el.addEventListener('close', () => {
-				document.body.contains(el) && document.body.removeChild(el)
-				el = null
-				clearTimeout(timer)
-				timer = setTimeout(() => {
-					onClose?.()
-				}, 50)
-			})
-			el.addEventListener('open', () => {
-				clearTimeout(timer)
-				timer = setTimeout(() => {
-					onOpen?.()
-				}, 50)
-			})
-			el.addEventListener('changevalue', (e: CustomEvent) => {
-				// if (onChange) {
-				// 	const err = onChange(e.detail)
-				// 	el['error'] = err || ''
-				// }
-				multipleInputs?.some((v) => {
-					if (v.label === e.detail?.label) {
-						v?.onChange?.(e.detail.value)
-						return true
-					}
-				})
-			})
-			el.addEventListener('tap', (e: CustomEvent) => {
-				// onConfirm && onConfirm()
-				buttons?.some((v) => {
-					if (v.label === e.detail?.label) {
-						v?.onTap?.()
-						return true
-					}
-				})
-			})
-			document.body.appendChild(el)
-		},
-		setInput(params: { label: string; type: string; v: any }) {
-			if (!multipleInputs) return
-			multipleInputs.some((v) => {
-				if (v.label === params.label) {
-					v[params.type] = params.v
-					return true
-				}
-			})
-			el?.setMultipleInputs?.(multipleInputs)
-		},
-		setButton(params: { label: string; type: string; v: any }) {
-			if (!buttons) return
-			buttons.some((v) => {
-				if (v.label === params.label) {
-					v[params.type] = params.v
-					return true
-				}
-			})
-			el?.setButton?.(buttons)
-		},
-		close() {
-			el?.close && el?.close()
-		},
-	}
-	return api
+  let el: any
+  const api = {
+    open() {
+      if (el) {
+        el.open()
+        return
+      }
+      el = document.createElement('saki-dialog-multiple-prompts')
+      el['title'] = title || ''
+      el['content'] = content || ''
+      el['autoHideDuration'] = autoHideDuration || 0
+      el['flexButton'] = flexButton
+      el['closeIcon'] = closeIcon
+      el?.setMultipleInputs?.(multipleInputs)
+      el?.setButton?.(buttons)
+      // el.addEventListener('load', () => {
+      // 	el.open()
+      // })
+      // el.onclose = () => {
+      //   console.log("close")
+      // 	document.body.removeChild(el)
+      // }
+      let timer: number
+      el.addEventListener('close', () => {
+        document.body.contains(el) && document.body.removeChild(el)
+        el = null
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+          onClose?.()
+        }, 50)
+      })
+      el.addEventListener('open', () => {
+        clearTimeout(timer)
+        timer = setTimeout(() => {
+          onOpen?.()
+        }, 50)
+      })
+      el.addEventListener('changevalue', (e: CustomEvent) => {
+        // if (onChange) {
+        // 	const err = onChange(e.detail)
+        // 	el['error'] = err || ''
+        // }
+        multipleInputs?.some((v) => {
+          if (v.label === e.detail?.label) {
+            v?.onChange?.(e.detail.value)
+            return true
+          }
+        })
+      })
+      el.addEventListener('clearvalue', (e: CustomEvent) => {
+        multipleInputs?.some((v) => {
+          if (v.label === e.detail?.label) {
+            v?.onClear?.()
+            return true
+          }
+        })
+      })
+      el.addEventListener('tap', (e: CustomEvent) => {
+        // onConfirm && onConfirm()
+        buttons?.some((v) => {
+          if (v.label === e.detail?.label) {
+            v?.onTap?.()
+            return true
+          }
+        })
+      })
+      document.body.appendChild(el)
+    },
+    setInput(params: { label: string; type: string; v: any }) {
+      if (!multipleInputs) return
+      multipleInputs.some((v) => {
+        if (v.label === params.label) {
+          v[params.type] = params.v
+          return true
+        }
+      })
+      el?.setMultipleInputs?.(multipleInputs)
+    },
+    addInput(params: MultipleInput) {
+      if (!multipleInputs) return
+      multipleInputs.push(params)
+      el?.setMultipleInputs?.(multipleInputs)
+    },
+    removeInput(label: string) {
+      if (!multipleInputs) return
+      multipleInputs = multipleInputs.filter((v) => v.label !== label)
+      el?.setMultipleInputs?.(multipleInputs)
+    },
+    setButton(params: { label: string; type: string; v: any }) {
+      if (!buttons) return
+      buttons.some((v) => {
+        if (v.label === params.label) {
+          v[params.type] = params.v
+          return true
+        }
+      })
+      el?.setButton?.(buttons)
+    },
+    close() {
+      el?.close && el?.close()
+    },
+  }
+  return api
 }
--- src/snackbar/index.ts
+++ src/snackbar/index.ts
@@ -1,64 +1,64 @@
 export const snackbar = (options: {
-	message: string
-	autoHideDuration?: number
-	vertical: 'bottom' | 'center' | 'top'
-	horizontal: 'left' | 'center' | 'right'
-	closeIcon?: boolean
-	backgroundColor?: string
-	backgroundHoverColor?: string
-	backgroundActiveColor?: string
-	color?: string
-	hoverColor?: string
-	activeColor?: string
-	fontWeight?: string
-	padding?: string
-	onTap?: () => void
+  message: string
+  autoHideDuration?: number
+  vertical: 'bottom' | 'center' | 'top'
+  horizontal: 'left' | 'center' | 'right'
+  closeIcon?: boolean
+  backgroundColor?: string
+  backgroundHoverColor?: string
+  backgroundActiveColor?: string
+  color?: string
+  hoverColor?: string
+  activeColor?: string
+  fontWeight?: string
+  padding?: string
+  onTap?: () => void
 }) => {
-	let el: any
-	const api = {
-		open() {
-			// console.log('state.app.status', el)
-			if (el) {
-				el.open()
-				return
-			}
-			el = document.createElement('saki-snackbar')
-			// console.log('state.app.status', el)
-			// console.log(el)
-			const { onTap } = options
-			Object.keys(options).forEach((k) => {
-				if (k != 'onTap' && options[k]) {
-					el[k] = options[k]
-				}
-			})
-			if (onTap) {
-				el['allowContentClick'] = 'true'
-				el.addEventListener('tap', () => {
-					onTap()
-				})
-			}
-			el.addEventListener('load', () => {
-				el.open()
-			})
-			el.addEventListener('close', () => {
-				document.body.contains(el) && document.body.removeChild(el)
-				el = null
-			})
-			document.body.appendChild(el)
-		},
-		close() {
-			el?.close && el?.close()
-		},
-		setMessage(msg: string) {
-			console.log('elelelel', el)
-			if (el) {
-				el['message'] = msg
-			} else {
-				options.message = msg
-			}
-		},
-	}
-	return api
+  let el: any
+  const api = {
+    open() {
+      // console.log('state.app.status', el)
+      if (el) {
+        el.open()
+        return
+      }
+      el = document.createElement('saki-snackbar')
+      // console.log('state.app.status', el)
+      // console.log(el)
+      const { onTap } = options
+      Object.keys(options).forEach((k) => {
+        if (k != 'onTap' && options[k]) {
+          el[k] = options[k]
+        }
+      })
+      if (onTap) {
+        el['allowContentClick'] = 'true'
+        el.addEventListener('tap', () => {
+          onTap()
+        })
+      }
+      el.addEventListener('load', () => {
+        el.open()
+      })
+      el.addEventListener('close', () => {
+        document.body.contains(el) && document.body.removeChild(el)
+        el = null
+      })
+      document.body.appendChild(el)
+    },
+    close() {
+      el?.close && el?.close()
+    },
+    setMessage(msg: string) {
+      // console.log('elelelel', el)
+      if (el) {
+        el['message'] = msg
+      } else {
+        options.message = msg
+      }
+    },
+  }
+  return api
 }
 // class Snackbar {
 // 	layouts: {
